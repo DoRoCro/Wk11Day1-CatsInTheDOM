1afc544f148f21106ca1a3cf88589e50261229b0
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-10 19:35:48 +0100

    move cats to array.forEach to display

--- public/app.js
+++ public/app.js
@@ -38,7 +38,19 @@ var addCat = function (name, food, urlForImage) {
 
 // get cats list section
 var catstuff = function () {
-  addCat('Fluffy', 'Anything small, cute and alive', 'https://s-media-cache-ak0.pinimg.com/736x/e1/c6/4b/e1c64b052929fd122bd8a0f532a7f1fe.jpg')
+  var cats = [
+    { name: 'Boba', food: 'Sock fluff', photo: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'
+    },
+    { name: 'Barnaby', food: 'Tuna', photo: 'http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg'
+    },
+    { name: 'Max', food: 'Whiskas Temptations', photo: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'
+    },
+    { name: 'Fluffy', food: 'Anything small, cute and alive', photo: 'https://s-media-cache-ak0.pinimg.com/736x/e1/c6/4b/e1c64b052929fd122bd8a0f532a7f1fe.jpg'
+    }
+  ]
+  cats.forEach(function (cat) {
+    addCat(cat.name, cat.food, cat.photo)
+  })
 }
 
 window.onload = catstuff
