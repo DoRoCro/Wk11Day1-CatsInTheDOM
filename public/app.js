366e96ce5a40cb1c511c5d5a242920a1015305a5
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-10 15:29:11 +0100

    add name to new ul

--- public/app.js
+++ public/app.js
@@ -4,11 +4,24 @@ var createUlForCat = function () {
   return ul
 }
 
-// get cats list section
-var catstuff = function () {
+var createNameLi = function (name) {
+  var li = document.createElement('li')
+  li.innerText = 'Name: ' + name
+  return li
+}
+
+var addCat = function (name) {
   var catsContainer = document.getElementById('cats')
   var ul = createUlForCat()
+  var nameLi = createNameLi(name)
+  ul.appendChild(nameLi)
+
   catsContainer.appendChild(ul)
+}
+
+// get cats list section
+var catstuff = function () {
+  addCat('Fluffy')
 }
 
 window.onload = catstuff
