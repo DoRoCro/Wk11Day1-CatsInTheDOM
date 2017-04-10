beaaf31fe55ac85a4186d442d9bdee9293f2a24b
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-10 20:26:55 +0100

    try some styling

--- public/app.js
+++ public/app.js
@@ -1,18 +1,25 @@
 var createUlForCat = function () {
   var ul = document.createElement('ul')
   ul.classList.add('cat')
+  ul.style.listStyleType = 'none'
+  document.body.style.backgroundColor = 'darkgray'
   return ul
 }
 
 var createNameLi = function (name) {
   var li = document.createElement('li')
   li.innerText = 'Name: ' + name
+  li.style.display = 'inline'
+  li.style.color = 'blue'
   return li
 }
 
 var createFoodLi = function (food) {
   var li = document.createElement('li')
   li.innerText = 'Favourite food: ' + food
+  li.style.color = 'darkcyan'
+  li.style.display = 'inline'
+  li.style.padding = '30px'
   return li
 }
 
@@ -20,6 +27,13 @@ var createImageLi = function (urlForImage) {
   var li = document.createElement('li')
   var img = document.createElement('img')
   img.style.width = '500px'
+  img.style.height = '500px'
+  img.style.display = 'inline-block'
+  img.style.margin = '5px'
+  img.style.border = '3px solid #c99'
+  img.style.backgroundPosition = 'center center'
+  img.style.backgroundSize = 'cover'
+  img.style.borderRadius = '50%'
   img.src = urlForImage
   li.appendChild(img)
   return li
@@ -46,6 +60,8 @@ var catstuff = function () {
     { name: 'Max', food: 'Whiskas Temptations', photo: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'
     },
     { name: 'Fluffy', food: 'Anything small, cute and alive', photo: 'https://s-media-cache-ak0.pinimg.com/736x/e1/c6/4b/e1c64b052929fd122bd8a0f532a7f1fe.jpg'
+    },
+    { name: 'Ginger', food: 'Cheese', photo: 'https://pbs.twimg.com/media/C75JBFeVsAAVwlj.jpg'
     }
   ]
   cats.forEach(function (cat) {
