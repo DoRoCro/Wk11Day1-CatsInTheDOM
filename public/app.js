8401e69c4f66a4f81d960d2034ec4e3353fb279a
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-10 15:47:29 +0100

    display image for new cat

--- public/app.js
+++ public/app.js
@@ -10,18 +10,35 @@ var createNameLi = function (name) {
   return li
 }
 
-var addCat = function (name) {
+var createFoodLi = function (food) {
+  var li = document.createElement('li')
+  li.innerText = 'Favourite food: ' + food
+  return li
+}
+
+var createImageLi = function (urlForImage) {
+  var li = document.createElement('li')
+  var img = document.createElement('img')
+  img.style.width = '500px'
+  img.src = urlForImage
+  li.appendChild(img)
+  return li
+}
+
+var addCat = function (name, food, urlForImage) {
   var catsContainer = document.getElementById('cats')
   var ul = createUlForCat()
   var nameLi = createNameLi(name)
   ul.appendChild(nameLi)
+  ul.appendChild(createFoodLi(food))
+  ul.appendChild(createImageLi(urlForImage))
 
   catsContainer.appendChild(ul)
 }
 
 // get cats list section
 var catstuff = function () {
-  addCat('Fluffy')
+  addCat('Fluffy', 'Anything small, cute and alive', 'https://s-media-cache-ak0.pinimg.com/736x/e1/c6/4b/e1c64b052929fd122bd8a0f532a7f1fe.jpg')
 }
 
 window.onload = catstuff
