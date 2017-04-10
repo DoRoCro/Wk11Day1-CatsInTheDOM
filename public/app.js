6bb6fe8733ad00fc7cbe705fbec9834a41d8cc36
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-10 15:22:23 +0100

    start creating a ul for next cat

--- public/app.js
+++ public/app.js
@@ -1,8 +1,14 @@
-// get cats list section
+var createUlForCat = function () {
+  var ul = document.createElement('ul')
+  ul.classList.add('cat')
+  return ul
+}
 
+// get cats list section
 var catstuff = function () {
-  var cats = document.getElementById('cats')
-  console.log(cats.length)
+  var catsContainer = document.getElementById('cats')
+  var ul = createUlForCat()
+  catsContainer.appendChild(ul)
 }
 
 window.onload = catstuff
